4c497fbc2f0e472974b642e6f1f607863220b65b
alifhs <alif.hs@outlook.com>
2021-11-29 13:49:47 +0600

    fireEvents

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 
 import App from "./App";
 
-
-describe('App', () => {
-  test('renders App component', async () => {
+describe("App", () => {
+  test("renders App component", async () => {
     render(<App />);
 
-    expect(screen.queryByText(/Signed in as/)).toBeNull();
-    screen.debug();
+    await screen.findByText(/Signed in as/);
 
-    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
-    screen.debug();
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: "JavaScript" },
+    });
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
   });
 });
